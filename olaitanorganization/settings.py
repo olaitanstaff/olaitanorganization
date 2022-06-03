f5aff1b3a4e360f3e5702ceb35e7c7da386937ff
JAZ1086 <43423892+JAZ1086@users.noreply.github.com>
2022-06-03 09:52:58 +0100

    changes to settings file

--- olaitanorganization/settings.py
+++ olaitanorganization/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECRET_KEY = 'django-insecure-kwba%$sa87m!*jcaoicbrp_dx-8jo@f$9*#*w(5ag66y!iwaec'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 

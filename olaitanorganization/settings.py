8f2217dde1225c56baf645b6a280fca3efcbc1f7
JAZ1086 <43423892+JAZ1086@users.noreply.github.com>
2022-06-03 01:37:08 +0100

    changed the setting file

--- olaitanorganization/settings.py
+++ olaitanorganization/settings.py
@@ -125,7 +125,8 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 STATICFILES_DIRS = [BASE_DIR / "static"]  
 STATIC_ROOT = STATIC_ROOT = BASE_DIR / "staticfiles" 
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+# STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
